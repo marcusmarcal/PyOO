59aca33326cb187a022d1ca3cf589d6646258001
Marcus Marçal <marcus.marcal@gmail.com>
2020-08-20 15:56:28 -0300

    Variáveis privadas em conta.py e exercício de formatação de data em datas.py

--- conta.py
+++ conta.py
@@ -1,23 +1,24 @@
 ## Testando POO em Python - Curso Alura
 ## Marcus Marçal - marcus.marcal@gmail.com
+## Branch DEV
 
 class Conta:
     def __init__(self, cpf, nome, saldo, limite = 1000.00):
-        self.nome = nome
-        self.cpf = cpf
-        self.saldo = saldo
-        self.limite = limite
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Nome do Cliente: {}".format(self.nome))
-        print("CPF: {}".format(self.cpf))
-        print("Saldo: {}".format(self.saldo))
-        print("Limite : {}".format(self.limite))
-        print("Saldo disponível: {}".format(self.saldo + self.limite))
+        print("Nome do Cliente: {}".format(self.__nome))
+        print("CPF: {}".format(self.__cpf))
+        print("Saldo: {}".format(self.__saldo))
+        print("Limite : {}".format(self.__limite))
+        print("Saldo disponível: {}".format(self.__saldo + self.__limite))
 
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
